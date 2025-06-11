2c5b86de76e2e0ee4c1401b01220278f766a54a7
✿ corey (they/them) <c@rey.foo>
2025-06-11 15:08:42 -0700

    Formatting

--- package.nix
+++ package.nix
@@ -3,24 +3,31 @@
   stdenv,
   pkgs,
   anemone,
-}:
-let
-  gitRev = if self ? rev then self.rev else "dirty";
-  patchGitRev = if gitRev != "dirty" then ''
-    sed -i "s|^#git_rev = .*|git_rev = \"${gitRev}\"|" config.toml
-  '' else "";
+}: let
+  gitRev =
+    if self ? rev
+    then self.rev
+    else "dirty";
+  patchGitRev =
+    if gitRev != "dirty"
+    then ''
+      sed -i "s|^#git_rev = .*|git_rev = \"${gitRev}\"|" config.toml
+    ''
+    else "";
 in
-stdenv.mkDerivation {
-  name = "zx.dev";
-  src = ./.;
-  buildInputs = [pkgs.zola];
-  buildPhase = "zola build";
-  installPhase = ''
-    mkdir -p $out
-    cp -r dist/* $out/
-  '';
-  postPatch = ''
-    mkdir -p themes/anemone
-    cp -r ${anemone}/. themes/anemone/
-  '' + patchGitRev;
-}
+  stdenv.mkDerivation {
+    name = "zx.dev";
+    src = ./.;
+    buildInputs = [pkgs.zola];
+    buildPhase = "zola build";
+    installPhase = ''
+      mkdir -p $out
+      cp -r dist/* $out/
+    '';
+    postPatch =
+      ''
+        mkdir -p themes/anemone
+        cp -r ${anemone}/. themes/anemone/
+      ''
+      + patchGitRev;
+  }
