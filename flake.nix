5ac1ef13e86b53b44c6e430b02d918b06445903b
✿ corey (they/them) <c@rey.foo>
2025-06-10 17:54:47 -0700

    Init flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} (top @ {
+      config,
+      withSystem,
+      moduleWithSystem,
+      ...
+    }: {
+      flake = {
+        nixConfig = {
+          experimental-features = ["nix-command" "flakes"];
+          extra-substituters = [
+            "https://stackptr.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "stackptr.cachix.org-1:5e2q7OxdRdAtvRmHTeogpgJKzQhbvFqNMmCMw71opZA="
+          ];
+        };
+      };
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {packages = [pkgs.just];};
+        formatter = pkgs.alejandra;
+      };
+    });
+}
